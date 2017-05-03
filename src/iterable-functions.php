f3d57d76f589590f7de99fd49ee4fc3e9dd9e532
Beno!t POLASZEK <benoit@synapse-interactive.fr>
2017-05-03 12:27:46 +0200

    Fix PHPDoc + PSR2

--- src/iterable-functions.php
+++ src/iterable-functions.php
@@ -4,7 +4,8 @@ if (!function_exists('is_iterable')) {
 
     /**
      * Check wether or not a variable is iterable (i.e array or \Traversable)
-     * @param array|\Traversable $iterable
+     *
+     * @param  array|\Traversable $iterable
      * @return bool
      */
     function is_iterable($iterable)
@@ -17,7 +18,8 @@ if (!function_exists('iterable_to_array')) {
 
     /**
      * Copy the iterable into an array. If the iterable is already an array, return it.
-     * @param array|\Traversable $iterable
+     *
+     * @param  array|\Traversable $iterable
      * @return array
      */
     function iterable_to_array($iterable)
@@ -27,15 +29,20 @@ if (!function_exists('iterable_to_array')) {
 }
 
 if (!function_exists('iterable_to_traversable')) {
+
+    /**
+     * If the iterable is not intance of \Traversable, it is an array => convert it to an ArrayIterator.
+     *
+     * @param  $iterable
+     * @return \Traversable
+     */
     function iterable_to_traversable($iterable)
     {
         if ($iterable instanceof Traversable) {
             return $iterable;
-        }
-        elseif (is_array($iterable)) {
+        } elseif (is_array($iterable)) {
             return new ArrayIterator($iterable);
-        }
-        else {
+        } else {
             throw new \InvalidArgumentException(sprintf('Expected array or \\Traversable, got %s', is_object($iterable) ? get_class($iterable) : gettype($iterable)));
         }
     }
