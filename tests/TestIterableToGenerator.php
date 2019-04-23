cb339cff457bb6aa067cbf29f3116646136d55e8
Merijn Wijngaard <mwijngaard@gmail.com>
2019-04-23 14:38:39 +0200

    Add iterable_to_generator function

---
+++ tests/TestIterableToGenerator.php
@@ -0,0 +1,57 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class TestIterableToGenerator extends TestCase
+{
+    protected function setUp()
+    {
+        if (version_compare(PHP_VERSION, '5.5') < 0) {
+            $this->markTestSkipped('generators are only available in PHP versions >= 5.5');
+        }
+    }
+
+    public function testFunctionExists()
+    {
+        $this->assertTrue(function_exists('iterable_to_generator'));
+    }
+
+    public function testIteratorToGenerator()
+    {
+        $iterator = new ArrayIterator(array('foo' => 'bar'));
+        $generator = iterable_to_generator($iterator);
+        $this->assertInstanceOf('Generator', $generator);
+        $this->assertEquals(array('foo' => 'bar'), iterable_to_array($generator));
+    }
+
+    public function testArrayToGenerator()
+    {
+        $array = array('foo' => 'bar');
+        $generator = iterable_to_generator($array);
+        $this->assertInstanceOf('Generator', $generator);
+        $this->assertEquals(array('foo' => 'bar'), iterable_to_array($generator));
+    }
+
+    public function testGeneratorToGenerator()
+    {
+        $generator = iterable_to_generator($this->createGenerator(array('foo' => 'bar')));
+        $this->assertInstanceOf('Generator', $generator);
+        $this->assertEquals(array('foo' => 'bar'), iterable_to_array($generator));
+    }
+
+    private function createGenerator(array $elements)
+    {
+        foreach ($elements as $key => $value) {
+            yield $key => $value;
+        }
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testInvalidArgument()
+    {
+        $gen = iterable_to_generator('foo');
+        $gen->valid();
+    }
+}
