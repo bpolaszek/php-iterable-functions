699d41d0e58017fe080343ed1b037fbf77fecd40
Simon Podlipsky <simon@podlipsky.net>
2020-05-01 10:10:58 +0200

    Drop deprecated functions `withMap()` and `withFilter()`

--- tests/TestIterableObject.php
+++ tests/TestIterableObject.php
@@ -43,38 +43,6 @@ class TestIterableObject extends \PHPUnit\Framework\TestCase
         $this->assertEquals($expectedResult, $iterableObject->asArray());
     }
 
-    public function testFilterMutator()
-    {
-        $filter = function ($value) {
-            return 'bar' === $value;
-        };
-        $iterableObject = iterable(array('foo', 'bar'))->withFilter($filter);
-        $this->assertEquals(array(1 => 'bar'), iterator_to_array($iterableObject));
-    }
-
-    public function testMapMutator()
-    {
-        $map = 'strtoupper';
-        $iterableObject = iterable(array('foo', 'bar'))->withMap($map);
-        $this->assertEquals(array('FOO', 'BAR'), iterator_to_array($iterableObject));
-    }
-
-    public function testFilterAndMapMutators()
-    {
-        $filter = function ($value) {
-            return 'bar' === $value;
-        };
-        $map = 'strtoupper';
-        $iterableObject = iterable(array('foo', 'bar'))->withMap($map)->withFilter($filter);
-        $this->assertEquals(array(1 => 'BAR'), iterator_to_array($iterableObject));
-        $iterableObject = iterable(array('foo', 'bar'))->map($map)->filter($filter);
-        $this->assertEquals(array(1 => 'BAR'), iterator_to_array($iterableObject));
-        $iterableObject = iterable(array('foo', 'bar'))->withFilter($filter)->withMap($map);
-        $this->assertEquals(array(1 => 'BAR'), iterator_to_array($iterableObject));
-        $iterableObject = iterable(array('foo', 'bar'))->filter($filter)->map($map);
-        $this->assertEquals(array(1 => 'BAR'), iterator_to_array($iterableObject));
-    }
-
     public function dataProvider()
     {
         $data = array('foo', 'bar');
